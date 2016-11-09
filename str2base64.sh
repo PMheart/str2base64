46cc499a3c28c458bb92a4d17b134f8e66803c40
PMheart <1286556770@qq.com>
2016-11-09 22:40:37 +0800

    Huge update.
    Add some new features. Hope you will like it :-) .

--- str2base64.sh
+++ str2base64.sh
@@ -1,27 +1,62 @@
 #!/bin/bash
 
+clear
+
 #
-# Script (str2base64.sh) to convert a string to base64 data in a way that it can be 
+# Script (str2base64.sh) to convert a string to base64 data in a way that it can be
 # used for properties as text <"desired text"> instead of <646573697265642074657874>
-# 
+#
 #
 # Version 1.1 - Copyright (c) 2013-2016 by Pike R. Alpha
+# Note: This is PMheart's fork version! i.e. This is NOT an offical version of str2base64.sh.
 #
 
-#
-# Get input from user.
+while true
+  do
+    #
+    # Get input from user.
+    #
+    read -p "Enter string to convert: " string
+    #
+    # Convert to postscript format.
+    #
+    data=$(echo -n "${string}"|xxd -ps|tr -d '\n')
+    #
+    # Add "00" to the data and convert it to base64 data.
+    #
+    output=$(echo -n "${data}00"|xxd -r -p|base64)
 
-#
-read -p "Enter string to convert: " string
+    #
+    # Want to exit? Or just convert 'exit' to base64?
+    #
+    if [[ "${string}" == "exit" ]];
+      then
+        read -p "You mean exiting (1) or convert 'exit' to base64 (2) ? (1/2) " todo
+        case "${todo}" in
+            1 )
+                #
+                # Exiting...
+                #
+                break
+            ;;
+            * )
+                #
+                # Print the base64 format of 'exit'. (ZXhpdAA=)
+                #
+                echo 'ZXhpdAA='
+                #
+                # Continue running the script.
+                #
+                continue
+            ;;
+        esac
+    fi
 
-#
-# Convert to postscript format.
-#
+    #
+    # Print the result after converting.
+    #
+    echo "${output}"
 
-data=$(echo -n "${string}"|xxd -ps|tr -d '\n')
+  done
 
-#
-# Add "00" to the data and convert it to base64 data.
-#
-
-echo -n "${data}00"|xxd -r -p|base64
+exit 0
